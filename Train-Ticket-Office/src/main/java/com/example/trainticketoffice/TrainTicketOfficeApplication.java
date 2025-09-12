1137d5e0e0b26d6423351307eed0b82dfb780699
LeHuyTuong <lehuytuong2005@gmail.com>
2025-09-12 17:34:37 +0700

    Init Project

---
+++ Train-Ticket-Office/src/main/java/com/example/trainticketoffice/TrainTicketOfficeApplication.java
@@ -0,0 +1,13 @@
+package com.example.trainticketoffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainTicketOfficeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainTicketOfficeApplication.class, args);
+    }
+
+}
---
+++ Train-Ticket-Office/src/test/java/com/example/trainticketoffice/TrainTicketOfficeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trainticketoffice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainTicketOfficeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
